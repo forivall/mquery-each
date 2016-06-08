64f2856b4dd9cf680fc7ecfc17a465a7efbe24c2
Jordan Klassen <jordan@klassen.me.uk>
2016-06-08 15:46:41 -0700

    feat: init implementation

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var isArray = require('lodash.isarray');
+var isObject = require('lodash.isobject');
+
+module.exports = function queryEach(query, fn) {
+  function processValue(key, value) {
+    var popped = {};
+    var context = {
+      pop: function (op) {
+        popped[op] = true;
+        return value[op];
+      }
+    };
+    if (isObject(value)) {
+      for (var op in value) {
+        if (!value.hasOwnProperty(op) || popped[op]) continue;
+        fn.call(context, key, value[op], op);
+      }
+    } else {
+      fn.call(context, key, value, '$eq');
+    }
+  }
+  function topLevel(query) {
+    for (var key in query) {
+      if (!query.hasOwnProperty(key)) continue;
+      var value = query[key];
+      if (Array.isArray(value)) {
+        for (var l = value.length, i = 0; i < l; i++) {
+          processValue(key, value[i]);
+        }
+      } else {
+        processValue(key, value);
+      }
+    }
+  }
+  if (Array.isArray(query)) {
+    for (var l = query.length, i = 0; i < l; i++) {
+      topLevel(query[i]);
+    }
+  } else {
+    topLevel(query);
+  }
+};
